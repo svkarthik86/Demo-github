09ab8eb0b30b96cd671bbbcf517686abc5f1535f
karthik <svkarthik86@gmail.com>
2021-12-16 16:53:01 +0530

    Demo test oop

---
+++ DemoGit.py
@@ -0,0 +1,14 @@
+class Students:
+    stdName = ""
+    stdRegNo = ""
+    stdadd = ""
+    def set_value(self, name, regno, add):
+        self.stdName = name
+        self.stdRegNo = regno
+        self.stdadd = add
+    def display(self):
+        print("Name:"+self.stdName + " " + "RegNo:" + self.stdRegNo + " " + "Address:" + self.stdadd)
+s1 = Students()
+s1.display()
+s1.set_value("Jhon", "1002", "Bangalore")
+s1.display()
